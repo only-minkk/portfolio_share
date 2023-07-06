807a15f6f0d43a566e90ae369ed973cf88e08df9
minkyu <tour00389@gmail.com>
2023-07-06 23:03:06 +0900

    Add 'likedBy' field referencing "User" to schema

--- back/src/db/schemas/board.js
+++ back/src/db/schemas/board.js
@@ -1,28 +1,32 @@
 import { Schema, model } from "mongoose";
 
-const BoardSchema = new Schema (
-    {
-        id: {
-            type: String,
-            required: true,
-        },
-        title: {
-            type: String,
-            required: true,
-        },
-        content: {
-            type: String,
-            required: true,
-        },
-        imgPath: {
-            type: String
-          },
+const BoardSchema = new Schema(
+  {
+    id: {
+      type: String,
+      required: true,
     },
-    {
-        timestamps: true,
-    }
+    title: {
+      type: String,
+      required: true,
+    },
+    content: {
+      type: String,
+      required: true,
+    },
+    imgPath: {
+      type: String,
+    },
+    likedBy: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
+  },
+  {
+    timestamps: true,
+  }
 );
 
-const BoardModel = model( "Board", BoardSchema);
+const BoardModel = model("Board", BoardSchema);
 
 export { BoardModel };
