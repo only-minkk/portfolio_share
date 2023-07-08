478977e411b49ed2e15762f465fc90036b062129
minkyu <tour00389@gmail.com>
2023-07-08 23:50:57 +0900

    Refactor : Update educationEditForm to send only modified data from the client to the server

--- front/src/components/education/EducationEditForm.js
+++ front/src/components/education/EducationEditForm.js
@@ -19,23 +19,47 @@ function EducationEditForm({ currentEducation, setEducations, setIsEditing }) {
     e.preventDefault();
     e.stopPropagation();
 
-    //curruentEducation의 user_id를 user_id 변수에 할당함.
-    const user_id = currentEducation.user_id;
+    // 변경사항을 담을 빈 객체 생성
+    const updatedEducation = {};
 
-    //"Educations/학력 id"엔드포인트로 PUT 요청함.
-    await Api.put(`educations/${currentEducation.id}`, {
-      user_id,
-      school,
-      major,
-      position,
-    });
+    // 변경된 school 필드 추가
+    if (school !== currentEducation.school) {
+      updatedEducation.school = school;
+    }
 
-    //"awardlist/유저id" 엔드포인트로 GET 요청함.
-    const res = await Api.get("educations", user_id);
-    //awards를 response의 data로 세팅함.
-    setEducations(res.data);
-    //편집 과정이 끝났으므로, isEditing을 false로 세팅함.
-    setIsEditing(false);
+    // 변경된 major 필드 추가
+    if (major !== currentEducation.major) {
+      updatedEducation.major = major;
+    }
+
+    // 변경된 position 필드 추가
+    if (position !== currentEducation.position) {
+      updatedEducation.position = position;
+    }
+
+    // 변경된 필드가 없는 경우, API 요청을 보내지 않음
+    if (Object.keys(updatedEducation).length === 0) {
+      console.log("변경사항 없습니다.");
+      setIsEditing(false);
+      return;
+    }
+
+    try {
+      //curruentEducation의 user_id를 user_id 변수에 할당함.
+      const user_id = currentEducation.user_id;
+
+      //"Educations/학력 id"엔드포인트로 PUT 요청함.
+      await Api.put(`educations/${currentEducation.id}`, updatedEducation);
+
+      //"awardlist/유저id" 엔드포인트로 GET 요청함.
+      const res = await Api.get("educations", user_id);
+      //awards를 response의 data로 세팅함.
+      setEducations(res.data);
+      //편집 과정이 끝났으므로, isEditing을 false로 세팅함.
+      setIsEditing(false);
+    } catch (error) {
+      console.log("에러발생", error);
+    }
   };
 
   return (
