9a07a03a3951ea8fbada6a5d79b78be36e95b1e0
minkyu <tour00389@gmail.com>
2023-07-08 22:47:53 +0900

    Revert "Refactor : Change update() method to save() method"
    This reverts commit d26a6ce641331aa77886e02b26140d43074fd662.

--- back/src/routers/userRouter.js
+++ back/src/routers/userRouter.js
@@ -90,7 +90,6 @@ userAuthRouter.put(
       // URI로부터 사용자 id를 추출함.
       const user_id = req.params.id;
       // body data 로부터 업데이트할 사용자 정보를 추출함.
-
       const name = req.body.name ?? null;
       const email = req.body.email ?? null;
       const password = req.body.password ?? null;
