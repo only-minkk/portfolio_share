201a005b160adf6cead4b105ebaa2bbce2d216ba
minkyu <tour00389@gmail.com>
2023-07-11 21:43:45 +0900

    Separate userAuthhandler middleware for improved modularity

---
+++ back/src/middlewares/userAuthHandler.js
@@ -0,0 +1,11 @@
+// 코드 중복을 줄이기 위한 try-catch 핸들러 생성
+const userAuthHandler = async (req, res, next, handler) => {
+  try {
+    const result = await handler(req);
+    res.status(200).json(result);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export { userAuthHandler };
--- back/src/routers/userRouter.js
+++ back/src/routers/userRouter.js
@@ -4,19 +4,10 @@ import {
   validateCreateUser,
   validateLoginUser,
 } from "../middlewares/userValidationMiddleware";
+import { userAuthHandler } from "../middlewares/userAuthHandler";
 import { userAuthService } from "../services/userService";
 
 const userAuthRouter = Router();
-
-// 코드 중복을 줄이기 위한 try-catch 핸들러 생성
-const userAuthHandler = async (req, res, next, handler) => {
-  try {
-    const result = await handler(req);
-    res.status(200).json(result);
-  } catch (error) {
-    next(error);
-  }
-};
 
 // 회원가입
 userAuthRouter.post(
