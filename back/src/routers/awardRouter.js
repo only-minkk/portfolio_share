af2ee01ddf493e42ea3b176b89321498cf6e0792
minkyu <tour00389@gmail.com>
2023-07-13 18:01:54 +0900

    Add tryCatchAsyncHandler to awardRouter to eliminate code duplication, improve readability

--- back/src/routers/awardRouter.js
+++ back/src/routers/awardRouter.js
@@ -1,74 +1,45 @@
-import is from "@sindresorhus/is";
 import { Router } from "express";
+import { awardValidate } from "../middlewares/awardValidationMiddleware";
+import { tryCatchAsycHandler } from "../middlewares/tryCatchAsycHandler";
 import { awardService } from "../services/awardService";
-import { ifErrorMessage } from "../middlewares/errorMiddleware";
 
 const awardRouter = Router();
 
 // 수상 내역 추가
-awardRouter.post("/award", async (req, res, next) => {
-  try {
-    if (is.emptyObject(req.body)) {
-      throw new Error(
-        "headers의 Content-Type을 application/json으로 설정해주세요"
-      );
-    }
-    const user_id = req.body.user_id;
-    const title = req.body.title;
-    const description = req.body.description;
-
-    const newAward = await awardService.addAward({
-      user_id,
-      title,
-      description,
-    });
-
-    ifErrorMessage(newAward);
-    res.status(201).json(newAward);
-  } catch (error) {
-    next(error);
-  }
+awardRouter.post("/award", awardValidate, async (req, res, next) => {
+  await tryCatchAsycHandler(req, res, next, (req) => {
+    const { user_id, title, description } = req.body;
+    const newAward = awardService.addAward({ user_id, title, description });
+    return newAward;
+  });
 });
 
 // 수상 내역 수정
 awardRouter.put("/awards/:id", async (req, res, next) => {
-  try {
+  await tryCatchAsycHandler(req, res, next, (req) => {
     const id = req.params.id;
-
     const toUpdate = req.body;
-    const updatedAward = await awardService.setAward({ id, toUpdate });
-
-    ifErrorMessage(updatedAward);
-    res.status(200).json(updatedAward);
-  } catch (error) {
-    next(error);
-  }
+    const updatedAward = awardService.setAward({ id, toUpdate });
+    return updatedAward;
+  });
 });
 
 // 수상 내역 조회
 awardRouter.get("/awards/:id", async (req, res, next) => {
-  try {
+  await tryCatchAsycHandler(req, res, next, (req) => {
     const user_id = req.params.id;
-    const awards = await awardService.getAwards({ user_id });
-
-    ifErrorMessage(awards);
-    res.status(200).send(awards);
-  } catch (error) {
-    next(error);
-  }
+    const awards = awardService.getAwards({ user_id });
+    return awards;
+  });
 });
 
 // 수상 내역 삭제
 awardRouter.delete("/awards/:id", async (req, res, next) => {
-  try {
+  await tryCatchAsycHandler(req, res, next, (req) => {
     const id = req.params.id;
-    const deletedAward = await awardService.deleteAward({ id });
-
-    ifErrorMessage(deletedAward);
-    res.send("삭제가 완료되었습니다.");
-  } catch (error) {
-    next(error);
-  }
+    const deletedAward = awardService.deleteAward({ id });
+    return deletedAward;
+  });
 });
 
 export { awardRouter };
