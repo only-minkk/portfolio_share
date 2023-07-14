9239450dafcb95e656dc13094bccdb94d5c91405
minkyu <tour00389@gmail.com>
2023-07-14 16:37:45 +0900

    refactor(awardService) : optimize setAward and improve readability

--- back/src/services/awardService.js
+++ back/src/services/awardService.js
@@ -2,6 +2,7 @@ import { Award } from "../db";
 import { v4 as uuidv4 } from "uuid";
 
 class awardService {
+  // 학력 등록
   static async addAward({ user_id, title, description }) {
     //   // id 는 유니크 값 부여
     const id = uuidv4();
@@ -15,6 +16,13 @@ class awardService {
     return createdNewAward;
   }
 
+  // 유저의 학력 조회
+  static async getAwards({ user_id }) {
+    const awards = await Award.findByUserId({ user_id });
+    return awards;
+  }
+
+  // 학력 수정
   static async setAward({ id, toUpdate }) {
     // 우선 해당 id 의 유저가 db에 존재하는지 여부 확인
     let award = await Award.findById({ id });
@@ -25,26 +33,31 @@ class awardService {
       return { errorMessage };
     }
 
-    if (toUpdate.title) {
-      const fieldToUpdate = "title";
-      const newValue = toUpdate.title;
-      award = await Award.update({ id, fieldToUpdate, newValue });
+    const fieldToUpdate = [];
+
+    // 변경된 필드의 key 값만 추출하여 fieldToUpdate 빈 배열에 push.
+    for (const key in toUpdate) {
+      fieldToUpdate.push(key);
     }
 
-    if (toUpdate.description) {
-      const fieldToUpdate = "description";
-      const newValue = toUpdate.description;
-      award = await Award.update({ id, fieldToUpdate, newValue });
+    // 모든 필드가 변경됐을 경우 save() 메서드로 한 번에 저장.
+    if (Object.keys(toUpdate).length === 2) {
+      award.title = toUpdate.title;
+      award.description = toUpdate.description;
+      const newAward = await award.save();
+      return newAward;
     }
+
+    // update() 메서드로 변경된 필드만 업데이트
+    fieldToUpdate.forEach((field) => {
+      const newValue = toUpdate[field];
+      award = Award.update({ id, fieldToUpdate: field, newValue });
+    });
 
     return award;
   }
 
-  static async getAwards({ user_id }) {
-    const awards = await Award.findByUserId({ user_id });
-    return awards;
-  }
-
+  // 학력 삭제
   static async deleteAward({ id }) {
     let awards = await Award.findById({ id });
 
