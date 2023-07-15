14d378fa577011d14b635294dabd1e18bd0240d0
minkyu <tour00389@gmail.com>
2023-07-15 17:46:54 +0900

    refactor `[certificateService]` : Optimize setCertificate using forEach( ) and save( ).

--- back/src/services/certificateService.js
+++ back/src/services/certificateService.js
@@ -2,6 +2,7 @@ import { Certificate } from "../db";
 import { v4 as uuidv4 } from "uuid";
 
 class certificateService {
+  // 자격증 추가
   static async addCertificate({ user_id, title, description, when_date }) {
     // id 는 유니크 값 부여
     const id = uuidv4();
@@ -15,11 +16,13 @@ class certificateService {
     return createdNewCertificate;
   }
 
+  // 자격증 조회
   static async getCertificates({ user_id }) {
     const certificates = await Certificate.findByUserId({ user_id });
     return certificates;
   }
 
+  // 자격증 수정
   static async setCertificate({ id, toUpdate }) {
     // 우선 해당 id 의 유저가 db에 존재하는지 여부 확인
     let certificate = await Certificate.findById({ id });
@@ -29,27 +32,31 @@ class certificateService {
       return { errorMessage };
     }
 
-    if (toUpdate.title) {
-      const fieldToUpdate = "title";
-      const newValue = toUpdate.title;
-      certificate = await Certificate.update({ id, fieldToUpdate, newValue });
+    const fieldToUpdate = [];
+
+    // 변경된 필드의 key 값만 추출하여 fieldToUpdate 빈 배열에 push
+    for (const key in toUpdate) {
+      fieldToUpdate.push(key);
     }
 
-    if (toUpdate.description) {
-      const fieldToUpdate = "description";
-      const newValue = toUpdate.description;
-      certificate = await Certificate.update({ id, fieldToUpdate, newValue });
+    // 모든 필드가 변경됐을 경우 save() 메서드로 한 번에 저장.
+    if (Object.keys(toUpdate).length === 3) {
+      certificate.title = toUpdate.title;
+      certificate.description = toUpdate.description;
+      certificate.when_date = toUpdate.when_date;
+      const newCertificate = await certificate.save();
+      return newCertificate;
     }
 
-    if (toUpdate.when_date) {
-      const fieldToUpdate = "when_date";
-      const newValue = toUpdate.when_date;
-      certificate = await Certificate.update({ id, fieldToUpdate, newValue });
-    }
-
-    return certificate;
+    // 변경된 필드만 update() 메서드로 업데이트
+    fieldToUpdate.forEach((field) => {
+      const newValue = toUpdate[field];
+      certificate = Certificate.update({ id, fieldToUpdate: field, newValue });
+      return certificate;
+    });
   }
 
+  // 자격증 삭제.
   static async deleteCertificate({ id }) {
     let certificates = await Certificate.findById({ id });
     if (!certificates) {
