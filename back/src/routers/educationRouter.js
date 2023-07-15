25266c8096588007a3137d42bf7917c0df9c05ca
minkyu <tour00389@gmail.com>
2023-07-15 16:24:41 +0900

    refactor `[educationRouter]` : Use tryCatchAsyncHandler to reduce duplication & Use validation

--- back/src/routers/educationRouter.js
+++ back/src/routers/educationRouter.js
@@ -1,73 +1,57 @@
 import { Router } from "express";
 import { tryCatchAsyncHandler } from "../middlewares/tryCatchAsyncHandler";
+import { educationValidate } from "../middlewares/educationValidation";
 import { educationService } from "../services/educationService";
 
 const educationRouter = Router();
 
 // 학력 정보 추가
-educationRouter.post("/education", async (req, res, next) => {
-  await tryCatchAsyncHandler;
-  // try {
-  //   const user_id = req.body.user_id;
-  //   const school = req.body.school;
-  //   const major = req.body.major;
-  //   const position = req.body.position;
-
-  //   const newEducation = await educationService.addEducation({
-  //     user_id,
-  //     school,
-  //     major,
-  //     position,
-  //   });
-
-  //   ifErrorMessage(newEducation);
-  //   res.status(201).json(newEducation);
-  // } catch (error) {
-  //   next(error);
-  // }
-});
+educationRouter.post(
+  "/education",
+  educationValidate,
+  async (req, res, next) => {
+    await tryCatchAsyncHandler(req, res, next, (req) => {
+      const { user_id, school, major, position } = req.body;
+      const newEducation = educationService.addEducation({
+        user_id,
+        school,
+        major,
+        position,
+      });
+      return newEducation;
+    });
+  }
+);
 
 // 학력 정보 수정
 educationRouter.put("/educations/:id", async (req, res, next) => {
-  try {
+  await tryCatchAsyncHandler(req, res, next, (req) => {
     const id = req.params.id;
     const toUpdate = req.body;
-    const updatedEducation = await educationService.setEducation({
+    const updatedEducation = educationService.setEducation({
       id,
       toUpdate,
     });
-
-    ifErrorMessage(updatedEducation);
-    res.status(200).json(updatedEducation);
-  } catch (error) {
-    next(error);
-  }
+    return updatedEducation;
+  });
 });
 
 // 학력 정보 조회
 educationRouter.get("/educations/:id", async (req, res, next) => {
-  try {
+  await tryCatchAsyncHandler(req, res, next, (req) => {
     const user_id = req.params.id;
-    const educations = await educationService.getEducations({ user_id });
-
-    ifErrorMessage(educations);
-    res.status(200).send(educations);
-  } catch (error) {
-    next(error);
-  }
+    const educations = educationService.getEducations({ user_id });
+    return educations;
+  });
 });
 
 // 학력 정보 삭제
 educationRouter.delete("/educations/:id", async (req, res, next) => {
-  try {
+  await tryCatchAsyncHandler(req, res, next, (req) => {
     const id = req.params.id;
-    const educations = await educationService.deleteEducation({ id });
-
-    ifErrorMessage(educations);
-    res.send("삭제가 완료되었습니다.");
-  } catch (error) {
-    next(error);
-  }
+    const deletedEducation = educationService.deleteEducation({ id });
+    return deletedEducation;
+  });
 });
 
 export { educationRouter };
