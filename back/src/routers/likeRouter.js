b5a6721e1097ed14b469a56b3a2bf754b7409336
minkyu <tour00389@gmail.com>
2023-07-20 16:10:06 +0900

    refactor `[likeRouter]` : Use tryCatchAsyncHandler to reduce duplication

--- back/src/routers/likeRouter.js
+++ back/src/routers/likeRouter.js
@@ -1,43 +1,50 @@
 import { Router } from "express";
+import { tryCatchAsyncHandler } from "../middlewares/tryCatchAsyncHandler";
 import { LikeModel } from "../db/schemas/like";
-import { ifErrorMessage } from "../middlewares/errorMiddleware";
 
 const likeRouter = Router();
 
-likeRouter.post("/like/uplike", (req, res) => {
-  let { commentId, userId } = req.body;
+likeRouter.post(
+  "/like/uplike",
+  tryCatchAsyncHandler(async (req, res) => {
+    let { commentId, userId } = req.body;
 
-  const LikeIns = new LikeModel({ userId, commentId });
+    const LikeIns = new LikeModel({ userId, commentId });
 
-  LikeIns.save((err, result) => {
-    if (err) return res.status(400).json({ upLike: false, err });
-    return res.status(200).json({ upLike: true });
-  });
-});
+    const upLike = await LikeIns.save();
 
-likeRouter.post("/like/unlike", (req, res) => {
-  let { commentId, userId } = req.body;
-
-  // console.log(commentId, userId);
-
-  LikeModel.findOneAndDelete({ commentId: commentId, userId: userId }).exec(
-    (err, result) => {
-      if (err) return res.status(400).json({ unLike: false, err });
-      return res.status(200).json({ unLike: true });
+    if (!upLike) {
+      throw new Error({ upLike: false, error });
     }
-  );
-});
+    return { uplike: true };
+  })
+);
 
-likeRouter.get("/like/getLike/:id", async (req, res, next) => {
-  try {
+likeRouter.post(
+  "/like/unlike",
+  tryCatchAsyncHandler(async (req) => {
+    const { commentId, userId } = req.body;
+
+    const like = await LikeModel.findOneAndDelete({
+      commentId: commentId,
+      userId: userId,
+    });
+
+    if (!like) {
+      throw new Error({ unlike: false, error });
+    }
+    return { unlike: true };
+  })
+);
+
+likeRouter.get(
+  "/like/getLike/:id",
+  tryCatchAsyncHandler(async (req) => {
     const userId = req.params.id;
-
     const likes = await LikeModel.find({ userId: userId });
-    ifErrorMessage(likes);
-    res.status(200).send(likes);
-  } catch (error) {
-    next(error);
-  }
-});
+
+    return likes;
+  })
+);
 
 export { likeRouter };
