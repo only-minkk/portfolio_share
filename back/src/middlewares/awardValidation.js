e03cbfc8a682dea3797546c279b740296342a842
minkyu <tour00389@gmail.com>
2023-07-25 23:39:43 +0900

    feat `[awardValidation]` : Add validation for empty fields in validation middleware

--- back/src/middlewares/awardValidation.js
+++ back/src/middlewares/awardValidation.js
@@ -1,7 +1,18 @@
 import { ValidationError } from "./CustomError";
 
 function validate(data, field) {
-  if (!data) {
+  // 올바른 필드명.
+  const correctField = ["title", "description"];
+
+  // 필드명이 올바른지 확인. 올바르지 않다면 에러.
+  if (!correctField.includes(field)) {
+    throw new ValidationError(
+      `${field}이란 필드명은 없습니다. 필드명을 다시 확인하여 주세요.`
+    );
+  }
+
+  // 필드에 값이 있는지 확인. 없으면 에러.
+  if (!data || data.trim() === "") {
     throw new ValidationError(`${field} 값을 입력해 주세요.`);
   }
 }
@@ -15,19 +26,17 @@ function awardValidate(req, res, next) {
 }
 
 function awardUpdateValidate(req, res, next) {
-  const fieldToUpdate = [];
+  const updateAward = req.body;
+  const updateField = [];
 
-  // 변경된 필드의 키 값을 빈 배열에 push.
-  for (const key in req.body) {
-    fieldToUpdate.push(key);
+  for (const field in updateAward) {
+    updateField.push(field);
   }
 
-  //변경된 필드의 값만 유효성 검사. 값이 비어있을 경우 에러.
-  fieldToUpdate.forEach((field) => {
-    if (req.body[field] === "") {
-      throw new ValidationError(`${field} 값을 입력해 주세요.`);
-    }
-  });
+  for (const field of updateField) {
+    validate(updateAward[field], field);
+  }
+
   next();
 }
 
