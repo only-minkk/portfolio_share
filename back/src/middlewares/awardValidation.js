f8e461c6256bd6e6f9ab92c3787814e9988199ea
minkyu <tour00389@gmail.com>
2023-07-26 16:37:14 +0900

    doc `[awardValidation]` : Add comments for improved readability

--- back/src/middlewares/awardValidation.js
+++ back/src/middlewares/awardValidation.js
@@ -1,5 +1,6 @@
 import { ValidationError } from "./CustomError";
 
+// 유효성 검사
 function validate(data, field) {
   // 올바른 필드명.
   const correctField = ["title", "description"];
@@ -11,12 +12,13 @@ function validate(data, field) {
     );
   }
 
-  // 필드에 값이 있는지 확인. 없으면 에러.
+  // 필드에 값이 없거만 빈 값이면 에러.
   if (!data || data.trim() === "") {
     throw new ValidationError(`${field} 값을 입력해 주세요.`);
   }
 }
 
+// 수상내역 추가 유효성 검사
 function awardValidate(req, res, next) {
   const { title, description } = req.body;
 
@@ -25,16 +27,22 @@ function awardValidate(req, res, next) {
   next();
 }
 
+// 수상내역 변경 유효성 검사
 function awardUpdateValidate(req, res, next) {
-  const updateAward = req.body;
-  const updateField = [];
+  // 변경 요청 값.
+  const awardToUpdate = req.body;
 
-  for (const field in updateAward) {
-    updateField.push(field);
+  // 변경할 필드를 넣을 빈 배열
+  const fieldToUpdate = [];
+
+  // 변경할 값의 필드를 빈 배열에 push.
+  for (const field in awardToUpdate) {
+    fieldToUpdate.push(field);
   }
 
-  for (const field of updateField) {
-    validate(updateAward[field], field);
+  // 변경할 필드를 토대로 유효성 검사.
+  for (const field of fieldToUpdate) {
+    validate(awardToUpdate[field], field);
   }
 
   next();
