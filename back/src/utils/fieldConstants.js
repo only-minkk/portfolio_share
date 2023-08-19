ae073f6d310111f33b10000b927dcfa968063bca
minkyu <tour00389@gmail.com>
2023-08-19 14:51:21 +0900

    refactor `[fieldConstant]` : Moved previously embedded constants to a separate file for centralized management

---
+++ back/src/utils/fieldConstants.js
@@ -0,0 +1,5 @@
+export const awardFields = ["title", "description"];
+export const certificateFields = ["title", "description", "when_date"];
+export const educationFields = ["school", "major", "position"];
+export const projectFields = ["title", "description", "from_date", "to_date"];
+export const userFields = ["name", "email", "description", "password"];
