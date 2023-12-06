43a4daafcbb07de8fbb27c4102c4fb32ceea0fb4
minkyu <tour00389@gmail.com>
2023-12-06 17:04:47 +0900

    refactor `[successMessage.js]` : Convert successMessage class to object literal
    - One of the reasons for using classes is to create object instances and reuse them multiple times. Therefore, I converted it to an object literal.

--- back/src/utils/successMessage.js
+++ back/src/utils/successMessage.js
@@ -1,13 +1,13 @@
-class successMessage {
-  static updateSuccessMessage = {
+const successMessage = {
+  updateSuccessMessage: {
     success: true,
     message: "업데이트에 성공하였습니다.",
-  };
+  },
 
-  static deleteSuccessMessage = {
+  deleteSuccessMessage: {
     success: true,
     message: "게시글이 성공적으로 삭제되었습니다.",
-  };
-}
+  },
+};
 
 export { successMessage };
