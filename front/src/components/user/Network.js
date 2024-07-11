7e74fd49b0547df3b8a342d56e6b491f6b7bd53b
minkyu <tour00389@gmail.com>
2024-07-11 20:01:30 +0900

    Revert "Fix for testing when network route request returns 404"
    This reverts commit 4db5e4f2ddef35049e561b53a64c289398bcbb20.

--- front/src/components/user/Network.js
+++ front/src/components/user/Network.js
@@ -23,7 +23,7 @@ function Network() {
       return;
     }
     // "userlist" 엔드포인트로 GET 요청을 하고, users를 response의 data로 세팅함.
-    Api.get("/userlist").then((res) => setUsers(res.data));
+    Api.get("userlist").then((res) => setUsers(res.data));
   }, [userState, navigate]);
 
   return (
